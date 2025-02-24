0f7f15ea6c5d98a674389dbdf30eb1ad07307f91
Robin Abela <Robin.Abela@etu.univ-smb.fr>
2025-02-24 17:43:58 +0100

    debut des tests

--- Api_Film_P1Tests/Controllers/UtilisateursControllerTests.cs
+++ Api_Film_P1Tests/Controllers/UtilisateursControllerTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Api_Film_P1.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +13,26 @@ namespace Api_Film_P1.Controllers.Tests
     [TestClass()]
     public class UtilisateursControllerTests
     {
+        //Ne pas les mettre dans le constructeurs
+        public UtilisateursControllerTests() 
+        {
+            var builder = new DbContextOptionsBuilder<FilmRatingsDBContext>()
+                        // Chaine de connexion à mettre dans les ( )
+                        .UseNpgsql("Server=localhost;port=5432;Database=TheFOOL; uid=postgres; password=postgres;"); 
+            FilmRatingsDBContext context = new FilmRatingsDBContext(builder.Options);
+            UtilisateursController controller = new UtilisateursController(context);
+        }
+        
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+
+        }
+
         [TestMethod()]
         public void GetUtilisateursTest()
         {
-
 
         }
     }
